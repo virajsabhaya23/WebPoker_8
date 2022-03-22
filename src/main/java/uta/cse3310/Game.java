d9a783864b94c7272239daf5ba736dfd74a39d68
bud davis <jimmie.davis@uta.edu>
2022-03-22 04:12:09 -0500

    added delete on close, update timer

--- src/main/java/uta/cse3310/Game.java
+++ src/main/java/uta/cse3310/Game.java
@@ -12,13 +12,21 @@ public class Game {
     ArrayList<Player> players = new ArrayList<>();
     int turn; // player ID that has the current turn
 
-    String exportStateAsJSON() {
+    public String exportStateAsJSON() {
         Gson gson = new Gson();
         return gson.toJson(this);
     }
 
     public void addPlayer(Player p) {
         players.add(p);
+    }
+
+    public void removePlayer(int playerid) {
+        // given it's player number, this method
+        // deletes the player from the players array
+        // and does whatever else is needed to remove
+        // the player from the game.
+        players.remove(playerid - 1);
     }
 
     public void processMessage(String msg) {
@@ -31,6 +39,18 @@ public class Game {
         if (event.event == UserEventType.NAME) {
             players.get(event.playerID).SetName(event.name);
         }
+
+    }
+
+    public boolean update() {
+
+
+        // this function is called on a periodic basis (once a second) by a timer
+        // it is to allow time based situations to be handled in the game
+        // if the game state is changed, it returns a true.
+        return false;
+        // expecting that returning a true will trigger a send of the game
+        // state to everyone
 
     }
 
